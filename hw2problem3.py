46b65fd951c3d9a6706d3e48a48d978fa20bacdb
Sohag Kumar Saha <103874802+sohagkumarsaha@users.noreply.github.com>
2023-02-09 16:16:40 -0600

    updated the codes

--- hw2problem3.py
+++ hw2problem3.py
@@ -16,6 +16,7 @@ N = 8
 n = np.arange(N)
 x = 0.1 * n
 
+# Define Function for Discrete Fourier Transform 
 def DFT(x):
     X = np.zeros(N, dtype=complex)
     for k in range(N):
@@ -23,6 +24,7 @@ def DFT(x):
             X[k] += x[n] * np.exp(-2j * np.pi * k * n / N) / np.sqrt(N)
     return X
 
+# Define Function for Discrete Consine Transform 
 def DCT(x):
     X = np.zeros(N)
     for k in range(N):
@@ -34,23 +36,41 @@ def DCT(x):
             X[k] += c * x[n] * np.cos(np.pi * (n + 0.5) * k / N)
     return X
 
+# Define Main function 
 def main(): 
 
     
-    X_f = DFT(x)
-    X_c = DCT(x)
+    X_f = DFT(x) # calling DFT function
+    X_c = DCT(x) # calling DCT function 
 
+    # Plot of DFT and DCT 
     plt.figure()
-    plt.stem(np.abs(X_f))
+    plt.stem(np.abs(X_f), label='X_f')
     plt.title("DFT Magnitude")
+    plt.xlabel('k')
+    plt.ylabel('|X_f [k]|')
+    plt.title('Magnitude of the DFT of x[n]')
+    plt.legend(title='$Magnitude-of-DFT$')
 
     plt.figure()
-    plt.stem(np.abs(X_c))
+    plt.stem(np.abs(X_c), label='X_c')
     plt.title("DCT Magnitude")
+    plt.xlabel('k')
+    plt.ylabel('|X_c [k]|')
+    plt.title('Magnitude of the DCT of x[n]')
+    plt.legend(title='$Magnitude-of-DCT$')
 
     plt.show()
+    
+    # Verification of Parsevals theorem 
+    Sq_DFT = np.sum(abs(X_f)**2) 
+    Sq_DCT = np.sum(abs(X_c)**2)
+    
+    if Sq_DFT == Sq_DCT:
+        print ('The parseval theorem is true!')
+    else:
+        print ('The parseval theorem is false!' )
     
     
 if __name__ == '__main__':
     main()
-    
