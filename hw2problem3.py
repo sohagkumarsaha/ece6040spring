ea645c2f395bae8b0a19c64d3d2892836601fb80
Sohag Kumar Saha <103874802+sohagkumarsaha@users.noreply.github.com>
2023-02-09 19:37:33 -0600

    added comments and units and legends

--- hw2problem3.py
+++ hw2problem3.py
@@ -21,7 +21,7 @@ def DFT(x):
     X = np.zeros(N, dtype=complex)
     for k in range(N):
         for n in range(N):
-            X[k] += x[n] * np.exp(-2j * np.pi * k * n / N) / np.sqrt(N)
+            X[k] += x[n] * (1/(np.sqrt(N)))  * np.exp((1j *2* np.pi * k * n) / N)
     return X
 
 # Define Function for Discrete Consine Transform 
@@ -31,9 +31,13 @@ def DCT(x):
         if k == 0:
             c = 1 / np.sqrt(N)
         else:
-            c = np.sqrt(2 / N)
+            c = np.sqrt(2 / N) 
+            #c = np.sqrt(2 / N) * np.cos(((np.pi)/N) * (n + 0.5) * k )
+            
         for n in range(N):
-            X[k] += c * x[n] * np.cos(np.pi * (n + 0.5) * k / N)
+            X[k] += x[n] * c * np.cos(((np.pi)/N) * (n + 0.5) * k )
+            #X[k] += x[n] * c 
+            
     return X
 
 # Define Main function 
@@ -43,20 +47,26 @@ def main():
     X_f = DFT(x) # calling DFT function
     X_c = DCT(x) # calling DCT function 
 
+
+    # For pretty looking fonts
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = 'Times New Roman'
+    plt.rcParams["mathtext.fontset"] = "cm"
+    
     # Plot of DFT and DCT 
     plt.figure()
     plt.stem(np.abs(X_f), label='X_f')
     plt.title("DFT Magnitude")
-    plt.xlabel('k')
-    plt.ylabel('|X_f [k]|')
+    plt.xlabel(r'k-th basis (Nos.)')
+    plt.ylabel(r'|X_f [k]| (a.u.)')
     plt.title('Magnitude of the DFT of x[n]')
     plt.legend(title='$Magnitude-of-DFT$')
 
     plt.figure()
     plt.stem(np.abs(X_c), label='X_c')
     plt.title("DCT Magnitude")
-    plt.xlabel('k')
-    plt.ylabel('|X_c [k]|')
+    plt.xlabel(r'k-th basis(Nos.)')
+    plt.ylabel(r'|X_c [k]| (a.u.)')
     plt.title('Magnitude of the DCT of x[n]')
     plt.legend(title='$Magnitude-of-DCT$')
 
